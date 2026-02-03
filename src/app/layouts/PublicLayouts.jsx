208ddefb6f8745b535b7befd9fa50682db3dc9e8
HelloWorld3605 <tuanhai362005t@gmail.com>
2026-02-03 09:35:08 +0700

    save

--- src/app/layouts/PublicLayouts.jsx
+++ src/app/layouts/PublicLayouts.jsx
@@ -4,7 +4,7 @@ import PublicHeader from "../../shared/components/Header/PublicHeader";
 import PublicFooter from "../../shared/components/PublicFooter/PublicFooter";
 import PublicArrowUp from "../../shared/components/ArrowUpTop/PublicArrowUp";
 import PublicSideBar from "../../shared/components/SideBar/PublicSideBar";
-import ScrollToTop from "../../shared/components/ScrollToTop";
+import ScrollToTop from "../../shared/utils/ScrollToTop";
 
 const PublicLayout = () => {
   return (
